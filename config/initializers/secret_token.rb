a7fbe5df32e8600defb59230cf977d6ef8382e6f
Bernardo Trindade <btrindade@eeshoo.com>
2013-08-26 18:36:45 -0600

    generated basic app

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+HouseList::Application.config.secret_token = '01406f5753427c0f56fee1400c447782e928f7f2d5bacb9907d87f3c2ba37c83c5b9941281872ad945ab14e037183ceec9063a5b78154bff1c1aad15fd6537f3'
